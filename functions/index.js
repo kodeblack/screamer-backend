f545eba1369477c579233cab4b0fe3d6d1c3af76
damilare <dblacktin@gmail.com>
2020-01-03 13:51:11 +0100

    notifications and triggers

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const app = require('express')();
 admin.initializeApp();
 
 const firebase_auth = require('./utility/firebase_auth');
+const { db } = require('./utility/admin')
 
 const {
   getScreams,
@@ -39,3 +40,67 @@ app.post('/user', firebase_auth, addUserDetails);
 app.get('/user', firebase_auth, getAUthenticatedUser);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions
+.region('europe-west1')
+.firestore.document('likes/{id}')
+.onCreate((snapshot) => {
+  return db
+  .doc(`/screams/${snapshot.data().screamId}`)
+  .get()
+  .then(doc => {
+    if(doc.exists){
+      return db.doc(`/notifications/${snapshot.id}`).set({
+        created_at: new Date().toISOString(),
+        recipient: doc.data().userHandle,
+        sender: snapshot.data().userHandle,
+        type: 'like',
+        read: false,
+        screamId: doc.id
+      })
+    }
+  })
+  .then(() => {
+    return;
+  })
+  .catch(err => {
+    console.error(err);
+    return;
+  })
+});
+
+exports.deleteNotificationOnUnlike = functions.region('europe-west1').firestore.document('likes/{id}')
+.onDelete((snapshot) => {
+  return db.doc(`/notifications/${snapshot.id}`).delete()
+  .then(() => {
+    return
+  })
+  .catch(err => {
+    console.error(err);
+    return
+  })
+});
+
+exports.createNotificationOnComment = functions.region('europe-west1').firestore.document('comments/{id}')
+.onCreate((snapshot) => {
+  return db.doc(`/screams/${snapshot.data().screamId}`).get()
+  .then(doc => {
+    if(doc.exists){
+      return db.doc(`/notifications/${snapshot.id}`).set({
+        created_at: new Date().toISOString(),
+        recipient: doc.data().userHandle,
+        sender: snapshot.data().userHandle,
+        type: 'comment',
+        read: false,
+        screamId: doc.id
+      })
+    }
+  })
+  .then(() => {
+    return;
+  })
+  .catch(err => {
+    console.error(err);
+    return;
+  })
+});
